042e1db7786f9d855056bdeda07a54b1cc3931dc
NiklasK2001 <niklask2001@gmail.com>
2017-08-15 17:10:59 +0200

    Finished Discord Bot

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const discord = require("discord.js");
 const bot = new discord.Client();
 const token = require("./token.js");
+var request = require('request');
 
 bot.on("ready", function(){
   console.log("Ready");
 });
+
 bot.on("message", function(message){
   if(message.author.equals(bot.user)) return;
   if(!message.content.startsWith("!profile")) return;
@@ -12,9 +14,19 @@ bot.on("message", function(message){
 
   if(content.length == 2){
     var profile = content[1];
-    console.log(profile);
-    message.channel.send("Getting Data for "+ profile+"...");
-  }
-});
+    message.channel.send("Predicting **"+ profile+"**...");
+    request('https://profile-o-mat.de:8080/predict?user='+profile, function (error, response, body) {
+      var result = JSON.parse(body);
+      var resultData = result.data;
+      var s = "Predictions for **"+profile+"**:\n```css";
+      for (var key in resultData) {
+        var resultInt = parseInt(resultData[key] * 100);
+        s += "\n["+key+"]: "+resultInt+"%";
+      }
+      s+="```";
+      message.channel.send(s)
+    });
+    }
+  });
 
 bot.login(token.TOKEN);
